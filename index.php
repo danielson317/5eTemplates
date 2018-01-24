15b27e1274c9be48f5e01b8d226e83e03ed0ca8c
Daniel P Henry <daniel@quickemr.com>
2018-01-24 11:38:50 -0700

    Reorganized.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-include('character.inc.php');
-include('websheet.inc.php');
-include('install.inc.php');
+// include('character.inc.php');
+// include('websheet.inc.php');
+// include('install.inc.php');
 
 echo 'Hello World';
 
-$create = new CreateDB('dnd.db');
-$create->createCharacterTable();
+// $create = new CreateDB('dnd.db');
+// $create->createCharacterTable();
 // Character Sheet
 // Spell Card
 // Moster Stats
