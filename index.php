4dabc721b25c50128168b253a16c908cd53bf3a8
Daniel P Henry <daniel@quickemr.com>
2018-01-24 11:51:13 -0700

    pointed index to character sheet.

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
+DEFINE('ROOT_PATH', __DIR__);
+
 // include('character.inc.php');
 // include('websheet.inc.php');
 // include('install.inc.php');
 
-echo 'Hello World';
+echo printCharacterSheet();
 
 // $create = new CreateDB('dnd.db');
 // $create->createCharacterTable();
 // Character Sheet
 // Spell Card
 // Moster Stats
+
+function printCharacterSheet()
+{
+  ob_start();
+  include ROOT_PATH . '/themes/default/templates/character.tpl.php';
+  return ob_get_clean();
+}
